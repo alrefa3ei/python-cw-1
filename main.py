663b4adfdb6a2818a4f059b7dfa8d193f8930044
alrefa3ei <alrefa3ei06@gmail.com>
2023-08-27 02:58:32 +0300

    Done

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 # Change the following print statement so that it uses the addition operator + and prints out 8
 
+Number_1 = 4
+
+Number_2 = 4
+
+sum = (Number_1 + Number_2)
+
+print (sum)
 # Write a print statement that uses the division operator / 
+
+Number_3 = 4
+
+Number_4 = 4
+
+sum_2 = ( Number_3 % Number_4)
+
+print (sum_2)
 
 # Add a print statement that uses multiplication *
 
+Number_5 = 4
+
+Number_6 = 2
+
+sum_3 = ( Number_5 * Number_6 )
+
+print (sum_3)
+
 # Create a variable called `length` and assign a value to it
+
+variable_length = 10
+
+variable_width = 20
+
+variable_area = ( variable_length * variable_width)
+
+VA = (variable_area)
+
+print (f"The result is {VA}")
 
 # Create a variable called    `width` and assign a value to it
 
@@ -14,3 +47,14 @@
 
 
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+
+rec_1 = 5
+
+rec_2 = 3
+
+rec_3 = 15
+
+print (f"If the length of the rectangle is {rec_1},and width is {rec_2},then the area is {rec_3}")
+
+
+
